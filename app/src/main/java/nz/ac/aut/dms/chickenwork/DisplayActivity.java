2782c6f6d50b290f1cf4fc384481bef09c084d3b
Guy Langford-lee <guy@langford-lee.com>
2015-05-07 16:01:02 +1200

    added stuff

--- app/src/main/java/nz/ac/aut/dms/chickenwork/DisplayActivity.java
+++ app/src/main/java/nz/ac/aut/dms/chickenwork/DisplayActivity.java
@@ -7,27 +7,35 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.app.Activity;
 import android.content.Intent;
+import android.view.View;
 import android.widget.TextView;
 
 public class DisplayActivity extends Activity {
 
-	private TextView textView;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_display);
-		textView = (TextView)findViewById(R.id.tagDisplay);
-		Intent intent = getIntent();
-		//Check mime type, get ndef message  from intent and display the message in text view
-        if(intent.getType() != null && intent.getType().equals("text/plain")) {
-        	Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+        textView = (TextView) findViewById(R.id.tagDisplay);
+        Intent intent = getIntent();
+        //Check mime type, get ndef message  from intent and display the message in text view
+        if (intent.getType() != null && intent.getType().equals("text/plain")) {
+            Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
             NdefMessage msg = (NdefMessage) rawMsgs[0];
             NdefRecord record = msg.getRecords()[0];
             String message = new String(record.getPayload());
             textView.setText(message);
         }
-		
-	}
 
+    }
+
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.btnSaveLocation){
+
+        }
+
+    }
 }
