e694d23927f93f5dcd27d795651f8d62ba18fefe
Guy Langford-lee <guy@langford-lee.com>
2015-05-07 23:58:07 +1200

    added http stuff

--- app/src/main/java/nz/ac/aut/dms/chickenwork/DisplayActivity.java
+++ app/src/main/java/nz/ac/aut/dms/chickenwork/DisplayActivity.java
@@ -1,5 +1,7 @@
 package nz.ac.aut.dms.chickenwork;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -7,8 +9,22 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DisplayActivity extends Activity implements View.OnClickListener{
 
@@ -40,5 +56,40 @@ public class DisplayActivity extends Activity implements View.OnClickListener{
 			Intent cameraintent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 			startActivityForResult(cameraintent, CAM_REQUEST);
 		}
+
+        if(view.getId() == R.id.btnSaveLocation){
+            LocationManager locationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+            if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                StrictMode.ThreadPolicy p = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(p);
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost("http://langford-lee.com:8080/c/addlocation");
+
+                try {
+                    String lat = String.valueOf(locationGPS.getLatitude());
+                    String lon = String.valueOf(locationGPS.getLongitude());
+
+
+                    // Add your data
+                    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                    nameValuePairs.add(new BasicNameValuePair("lat", lat));
+                    nameValuePairs.add(new BasicNameValuePair("lon", lon));
+                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                    // Execute HTTP Post Request
+                    HttpResponse responce = httpclient.execute(httppost);
+                    System.out.println();
+                    System.out.println(lon);
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                }
+            }else{
+                Toast.makeText(this, "GPS Must Be Off turn it on and Try Again", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        }
 	}
 }
